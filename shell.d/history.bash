7ea374dd1d92fb80832e4bdeb2c784e5d8cef47f
Matthew Dargan <matthewdargan57@gmail.com>
2021-03-11 00:03:11 -0600

    Start adding bash shell customizations. TODO add autocompletion

---
+++ shell.d/history.bash
@@ -0,0 +1,4 @@
+set -o vi
+export HISTCONTROL=ignoreboth
+export HISTSIZE=5000
+export HISTFILESIZE=10000
---
+++ shell.d/path.sh
@@ -0,0 +1,24 @@
+export SCRIPTS=$REPOS/github.com/$GITUSER/dotfiles/scripts
+
+export PATH=\
+$SCRIPTS:\
+$HOME/.local/bin:\
+$HOME/.local/go/bin:\
+$HOME/.cargo/bin:\
+/usr/local/go/bin:\
+/usr/local/bin:\
+/usr/local/sbin:\
+/usr/sbin:\
+/usr/bin:\
+/snap/bin:\
+/sbin:\
+/bin
+
+# Be sure not to remove ./ or stuff gets weird.
+export CDPATH=\
+./:\
+$REPOS/github.com/$GITUSER:\
+$REPOS/github.com/*:\
+$REPOS/github.com:\
+$REPOS:\
+$HOME
