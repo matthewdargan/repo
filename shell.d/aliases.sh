ba746498b4e7f9e424a488169393ba20c667ba3c
Matthew Dargan <matthewdargan57@gmail.com>
2021-03-11 17:34:15 -0600

    Finish the rest of the shell extensions

---
+++ shell.d/aliases.sh
@@ -0,0 +1,13 @@
+unalias -a
+
+alias grep='grep -i --colour=auto'
+alias egrep='egrep -i --colour=auto'
+alias fgrep='fgrep -i --colour=auto'
+alias curl='curl -L'
+alias ls='ls -h --color=auto'
+alias free='free -h'
+alias df='df -h'
+alias top="htop"
+
+which nvim &>/dev/null && alias vi=nvim
+which nvim &>/dev/null && alias vim=nvim
---
+++ shell.d/golang.sh
@@ -0,0 +1,5 @@
+export GOPRIVATE="github.com/$GITUSER/*"
+export GOBIN="/usr/local/go/bin"
+mkdir -p $GOBIN
+export GOPATH="/usr/local/go"
+export GOPROXY="direct"
