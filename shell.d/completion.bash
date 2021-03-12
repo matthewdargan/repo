a7daafe4463b15012bf43620fd229595da87393c
Matthew Dargan <matthewdargan57@gmail.com>
2021-03-11 18:48:37 -0600

    Add history search autocompletion

--- shell.d/completion.bash
+++ shell.d/completion.bash
@@ -1,3 +1,8 @@
 if [ -r /usr/share/bash-completion/bash_completion ]; then
 	source /usr/share/bash-completion/bash_completion
 fi
+
+if [[ $- == *i* ]]; then
+    bind '"\e[A": history-search-backward'
+    bind '"\e[B": history-search-forward'
+fi
