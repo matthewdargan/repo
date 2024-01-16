5c8c284e7475a230bc3189c5387d51e6de868547
Matthew Dargan <matthewdargan57@gmail.com>
2024-01-15 23:32:16 -0600

    nvim: add lsp settings

---
+++ nvim/lua/mpd/lsp.lua
@@ -0,0 +1,90 @@
+local cmp = require("cmp")
+cmp.setup({
+    snippet = {
+        expand = function(args)
+            require("luasnip").lsp_expand(args.body)
+        end,
+    },
+    mapping = {
+        ["<Tab>"] = function(fallback)
+            if cmp.visible() then
+                cmp.select_next_item()
+            else
+                fallback()
+            end
+        end,
+        ["<CR>"] = function(fallback)
+            if cmp.visible() then
+                cmp.confirm()
+            else
+                fallback()
+            end
+        end
+    },
+    sources = cmp.config.sources({
+        { name = "buffer" },
+        { name = "nvim_lsp" },
+        { name = "luasnip" },
+    })
+})
+
+require("mason").setup()
+local capabilities = require("cmp_nvim_lsp").default_capabilities()
+local on_attach = function(_, _)
+    local builtin = require("telescope.builtin")
+    vim.keymap.set("n", "gd", builtin.lsp_definitions)
+    vim.keymap.set("n", "gi", builtin.lsp_implementations)
+    vim.keymap.set("n", "gr", builtin.lsp_references)
+    vim.keymap.set("n", "gt", builtin.lsp_type_definitions)
+    vim.keymap.set("n", "K", vim.lsp.buf.hover)
+    vim.keymap.set("n", "<leader>r", vim.lsp.buf.rename)
+
+    vim.api.nvim_create_autocmd("BufWritePre", {
+        callback = function()
+            vim.lsp.buf.format { async = false }
+        end
+    })
+end
+
+local lspconfig = require("lspconfig")
+lspconfig.gopls.setup {
+    capabilities = capabilities,
+    on_attach = on_attach,
+    settings = {
+        gopls = {
+            gofumpt = true,
+        },
+    },
+}
+lspconfig.golangci_lint_ls.setup {
+    capabilities = capabilities,
+    on_attach = on_attach,
+    settings = {
+        gopls = {
+            gofumpt = true,
+        },
+    },
+}
+lspconfig.jsonls.setup {
+    capabilities = capabilities,
+    on_attach = on_attach,
+}
+lspconfig.lua_ls.setup {
+    capabilities = capabilities,
+    on_attach = on_attach,
+    settings = {
+        Lua = {
+            diagnostics = {
+                globals = { "vim" },
+            },
+        },
+    },
+}
+lspconfig.terraformls.setup {
+    capabilities = capabilities,
+    on_attach = on_attach,
+}
+lspconfig.yamlls.setup {
+    capabilities = capabilities,
+    on_attach = on_attach,
+}
--- nvim/lua/mpd/plugins.lua
+++ nvim/lua/mpd/plugins.lua
@@ -1,19 +1,30 @@
 return {
-    { "rebelot/kanagawa.nvim", name = "kanagawa", priority = 1000 },
+    {
+        "rebelot/kanagawa.nvim",
+        name = "kanagawa",
+        priority = 1000
+    },
     {
         "nvim-treesitter/nvim-treesitter",
         build = ":TSUpdate",
-        config = function () 
+        config = function()
             local configs = require("nvim-treesitter.configs")
             configs.setup({
                 ensure_installed = { "go", "json", "lua", "terraform", "yaml" },
                 sync_install = false,
                 highlight = { enable = true },
-                indent = { enable = true },  
+                indent = { enable = true },
             })
         end
     },
     { "williamboman/mason.nvim" },
     { "neovim/nvim-lspconfig" },
     { "nvim-telescope/telescope.nvim", dependencies = { "nvim-lua/plenary.nvim" } },
+    { "hrsh7th/nvim-cmp" },
+    { "hrsh7th/cmp-nvim-lsp" },
+    { "hrsh7th/cmp-buffer" },
+    { "hrsh7th/cmp-path" },
+    { "hrsh7th/cmp-cmdline" },
+    { "L3MON4D3/LuaSnip" },
+    { "saadparwaiz1/cmp_luasnip" },
 }
