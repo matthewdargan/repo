7a183198270aabe735f68591ac5c90ad6eab9a58
Matthew Dargan <matthewdargan57@gmail.com>
2024-03-05 21:23:24 -0600

    feat: migrate bash config to nix

--- .profile
+++
@@ -1,7 +0,0 @@
-# This file is only here because it is required by some applications.
-if [ -n "$BASH_VERSION" ]; then
-  if [ -f "$HOME/.bashrc" ]; then
-    . "$HOME/.bashrc"
-  fi
-fi
-
--- home.nix
+++ home.nix
@@ -9,9 +9,27 @@
   home.packages = [pkgs.alejandra pkgs.go_1_22 pkgs.terraform];
   nixpkgs.config.allowUnfree = true;
   programs = {
+    bash = {
+      enable = true;
+      historyControl = ["ignoredups" "ignorespace"];
+      sessionVariables = {
+        PATH = "$HOME/bin:$GOPATH/bin:$PATH";
+        EDITOR = "nvim";
+        VISUAL = "nvim";
+      };
+      shellAliases = {
+        ls = "ls -h --color=auto";
+        ll = "ls -alF";
+        vim = "nvim";
+      };
+      initExtra = ''
+        bind '"\e[A": history-search-backward'
+        bind '"\e[B": history-search-forward'
+        set -o vi
+      '';
+    };
     direnv = {
       enable = true;
-      enableBashIntegration = true;
       nix-direnv.enable = true;
     };
   };
