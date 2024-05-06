25291ddf383c9c604358a4170d45616882ef6946
Matthew Dargan <matthewdargan57@gmail.com>
2024-05-06 17:04:44 -0500

    feat: add nh to devShell

--- parts/default.nix
+++ parts/default.nix
@@ -6,7 +6,7 @@ _: {
     ...
   }: {
     devShells.default = pkgs.mkShell {
-      packages = [inputs'.home-manager.packages.home-manager];
+      packages = [inputs'.home-manager.packages.home-manager pkgs.nh];
       shellHook = "${config.pre-commit.installationScript}";
     };
     pre-commit = {
