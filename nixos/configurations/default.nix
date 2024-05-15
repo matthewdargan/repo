0cf517a53f9da168e68c2bf7c34d42ab3161d4b4
Matthew Dargan <matthewdargan57@gmail.com>
2024-05-14 23:17:03 -0500

    style: nixos/configurations/default.nix

--- nixos/configurations/default.nix
+++ nixos/configurations/default.nix
@@ -1,8 +1,6 @@
 {inputs, ...}: {
-  flake.nixosConfigurations = {
-    scoop = inputs.nixpkgs.lib.nixosSystem {
-      modules = [(import ./scoop inputs) ../../modules/settings.nix];
-      system = "x86_64-linux";
-    };
+  flake.nixosConfigurations.scoop = inputs.nixpkgs.lib.nixosSystem {
+    modules = [(import ./scoop inputs) ../../modules/settings.nix];
+    system = "x86_64-linux";
   };
 }
