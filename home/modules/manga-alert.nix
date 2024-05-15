ae153a5190b436c06d3c2df6e7d963eb4528fd10
Matthew Dargan <matthewdargan57@gmail.com>
2024-05-15 09:07:42 -0500

    refactor: use home username in manga-alert service

--- home/modules/manga-alert.nix
+++ home/modules/manga-alert.nix
@@ -1,8 +1,8 @@
-_: {
+{config, ...}: {
   manga-alert = {
     enable = true;
     manga = ["one piece"];
     timer.enable = true;
-    user = "mpd";
+    user = "${config.home.username}";
   };
 }
