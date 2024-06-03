f0c47d8a4d40cf7d0247a973fff2d55ce0dbfd4b
Matthew Dargan <matthewdargan57@gmail.com>
2024-06-02 21:22:09 -0500

    fix: add nas to nixos configs

--- nixos/configurations/default.nix
+++ nixos/configurations/default.nix
@@ -1,6 +1,12 @@
 {inputs, ...}: {
-  flake.nixosConfigurations.scoop = inputs.nixpkgs.lib.nixosSystem {
-    modules = [(import ./scoop inputs) ../../modules/settings.nix];
-    system = "x86_64-linux";
+  flake.nixosConfigurations = {
+    nas = inputs.nixpkgs.lib.nixosSystem {
+      modules = [(import ./nas inputs) ../../modules/settings.nix];
+      system = "x86_64-linux";
+    };
+    scoop = inputs.nixpkgs.lib.nixosSystem {
+      modules = [(import ./scoop inputs) ../../modules/settings.nix];
+      system = "x86_64-linux";
+    };
   };
 }
