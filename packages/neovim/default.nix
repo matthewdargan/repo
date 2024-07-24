ee3d61367cb6b5607b8f53d48cb50f948592a509
Matthew Dargan <matthewdargan57@gmail.com>
2024-07-23 22:57:19 -0500

    refactor(neovim): cmp remappings

--- packages/neovim/default.nix
+++ packages/neovim/default.nix
@@ -55,9 +55,9 @@ _: {
               enable = true;
               settings = {
                 mapping = {
-                  "<CR>" = "cmp.mapping.confirm({ select = true })";
-                  "<S-Tab>" = "cmp.mapping(cmp.mapping.select_prev_item(), {'i', 's'})";
-                  "<Tab>" = "cmp.mapping(cmp.mapping.select_next_item(), {'i', 's'})";
+                  "<C-n>" = "cmp.mapping.select_next_item { behavior = cmp.SelectBehavior.Insert }";
+                  "<C-p>" = "cmp.mapping.select_prev_item { behavior = cmp.SelectBehavior.Insert }";
+                  "<C-y>" = ''cmp.mapping(cmp.mapping.confirm { behavior = cmp.ConfirmBehavior.Insert, select = true }, { "i", "c" })'';
                 };
                 snippet.expand = "function(args) require('luasnip').lsp_expand(args.body) end";
                 sources = [
