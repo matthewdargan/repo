302ad83ab38e71b7f554f63bb78cac3e46318729
Matthew Dargan <matthewdargan57@gmail.com>
2024-09-19 20:17:52 -0500

    refactor(wezterm): Comic Mono font

--- home/modules/wezterm.nix
+++ home/modules/wezterm.nix
@@ -1,11 +1,12 @@
 {inputs, ...}: {pkgs, ...}: {
+  home.packages = [pkgs.comic-mono];
   programs.wezterm = {
     enable = true;
     extraConfig = ''
       local wezterm = require 'wezterm'
       local config = wezterm.config_builder()
       config.color_scheme = 'tokyonight_night'
-      config.font = wezterm.font('PragmataPro Mono')
+      config.font = wezterm.font('Comic Mono')
       config.font_size = 20.0
       config.hide_tab_bar_if_only_one_tab = true
       local mux = wezterm.mux
