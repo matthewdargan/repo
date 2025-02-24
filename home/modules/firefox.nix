bd1b77cfb1cd1ff2d5e850f73a1b9e47d808f22c
Matthew Dargan <matthewdargan57@gmail.com>
2025-02-23 21:24:47 -0600

    build(nix): nix flake update

--- home/modules/firefox.nix
+++ home/modules/firefox.nix
@@ -6,7 +6,7 @@
   programs.firefox = {
     enable = true;
     package = pkgs.firefox;
-    profiles.${config.home.username}.extensions = with inputs.firefox-addons.packages.${pkgs.system}; [
+    profiles.${config.home.username}.extensions.packages = with inputs.firefox-addons.packages.${pkgs.system}; [
       bitwarden
       darkreader
       ublock-origin
